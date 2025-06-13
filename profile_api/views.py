fca58ce9b65af46d883d9d324dddce99acbda82b
Julius Pabular <julius.pabular@gmail.com>
2025-06-14 04:17:36 +0800

    Add `HelloSerializer` and implement POST method in `HelloApiView`.

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,17 +1,35 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from profile_api import serializers
 
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apiview = [
-            'Uses HTTP methods as function (get, post, patch, put, delete)',
-            'Is similar to a traditional Django View',
-            'Gives you the most control over your application logic',
-            'Is mapped manually to URLs',
+            "Uses HTTP methods as function (get, post, patch, put, delete)",
+            "Is similar to a traditional Django View",
+            "Gives you the most control over your application logic",
+            "Is mapped manually to URLs",
         ]
 
-        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+        return Response({"message": "Hello!", "an_apiview": an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Hello {name}"
+            return Response({"message": message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
