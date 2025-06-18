434c8324723e6840ce9ae87044f12575771ed133
Julius Pabular <julius.pabular@gmail.com>
2025-06-19 02:23:14 +0800

    Add `UserProfileViewSet` with serializer and URL routing for managing user profiles

--- profile_api/views.py
+++ profile_api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status, viewsets
 
 from profile_api import serializers
-
+from profile_api import models
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -90,3 +90,9 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({"http_method": "DELETE"})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
