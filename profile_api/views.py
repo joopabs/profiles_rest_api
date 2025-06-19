06f96a439e0d877a1d5a6ad3b81c7f9ca5127083
Julius Pabular <julius.pabular@gmail.com>
2025-06-20 03:04:25 +0800

    Add custom permission to allow users to edit their own profiles and integrate search functionality in `UserProfileViewSet`

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,10 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profile_api import serializers
 from profile_api import models
-
+from profile_api import permissions
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -91,6 +93,9 @@ class HelloViewSet(viewsets.ViewSet):
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
-
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name", "email",)
