8c6363c95cbad21388be47a8d0e3c0465880f3e6
Julius Pabular <julius.pabular@gmail.com>
2025-06-21 21:43:00 +0800

    Update `ProfileFeedViewSet` to enforce `IsAuthenticated` permission instead of `IsAuthenticatedOrReadOnly`

--- profile_api/views.py
+++ profile_api/views.py
@@ -5,7 +5,7 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from profile_api import serializers
 from profile_api import models
@@ -116,7 +116,7 @@ class UserProfileFeedViewSet(viewsets.ModelViewSet):
     queryset = models.ProfileFeedItem.objects.all()
     permission_classes = (
         permissions.UpdateOwnStatus,
-        IsAuthenticatedOrReadOnly
+        IsAuthenticated
     )
 
     def perform_create(self, serializer):
